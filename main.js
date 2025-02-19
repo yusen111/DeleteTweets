2a2138d4101c0f97ab2c3b18393826dbd7a45c58
yusen111 <sjgim0403@gmail.com>
2025-02-19 18:21:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var authorization = "Bearer ***"; // replace by authorization value
+var authorization = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA
+"; // replace by authorization value
 var ua = navigator.userAgentData.brands.map(brand => `"${brand.brand}";v="${brand.version}"`).join(', ');
-var client_tid = "***"; // replace by X-Client-Transaction-Id value
+var client_tid = "8Bb6Fdtv0uDrvhJulZ/MNbZ0pig4Kq+OoM3Uf8Pk5MGZDlLNbEH6vYWsErzay/lQayralvO2VHMMjDfeCQKUXBgTHCxT8w"; // replace by X-Client-Transaction-Id value
 var client_uuid = "***"; // replace by X-Client-Uuid value
 var csrf_token = getCookie("ct0");
 var random_resource = "uYU5M2i12UhDvDTzN6hZPg";
@@ -8,7 +9,7 @@ var random_resource_old_tweets = "H8OOoI-5ZE4NxgRr8lfyWg"
 var language_code = navigator.language.split("-")[0]
 var tweets_to_delete = []
 var user_id = getCookie("twid").substring(4);
-var username = "YourUsernameHere" // replace with your username
+var username = "@R0ckstar_111" // replace with your username
 var stop_signal = undefined
 var twitter_archive_content = undefined
 var twitter_archive_loading_confirmed = false
@@ -62,6 +63,8 @@ var delete_options = {
 	*/
 	"after_date":new Date('1900-01-01'), // year-month-day
 	"before_date":new Date('2100-01-01') // year-month-day
+
+	delete_message_without_media_only ":false
 }
 
 function buildAcceptLanguageString() {
